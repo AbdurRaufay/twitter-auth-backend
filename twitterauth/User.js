9c8d8398cbd3e1cd27390261f578c1283fec6912
abdurrauf <abdurrauf0306@gmail.com>
2023-08-02 18:15:49 -0700

    l

---
+++ twitterauth/User.js
@@ -0,0 +1,12 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  twitterId: String,
+  username: String,
+  name: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ twitterauth/passport.js
@@ -0,0 +1,50 @@
+// twitterauth/passport.js
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const User = require('./User');
+
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: '0V3aAF0k6f3cGMdltbGvsLgxC',
+      consumerSecret: 'jX3a7wHQShEMNWMbwrwqlVilguecgciOmtjgpa6qzvVimsrONO',
+      callbackURL: 'http://localhost:8080/auth/twitter/callback',
+    },
+    async (token, tokenSecret, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ twitterId: profile.id });
+        console.log(existingUser, "ex");
+        if (existingUser) {
+          return done(null, existingUser);
+        } else {
+          // Create a new user if not found in the database
+          const newUser = new User({
+            twitterId: profile.id,
+            username: profile.username,
+            name: profile.displayName,
+          });
+
+          const savedUser = await newUser.save();
+          return done(null, savedUser);
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); // Use user ID for serialization
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport;
